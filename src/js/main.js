b7ceb24b0cbe381d723a71d3b49319bc564bbe46
arkadii.vodolazskyi <49953024+avcat@users.noreply.github.com>
2024-10-24 22:43:56 +0300

    Moved listeners into single document event listener within IIFEs.

--- src/js/main.js
+++ src/js/main.js
@@ -1,70 +1,74 @@
 import customizations from '../data/customizations.js';
 import Fieldset from "../components/Fieldset.js";
 
-// Output customization fields
 document.addEventListener('DOMContentLoaded', () => {
-  const customizationForm = document.querySelector('[data-js="customize"]');
-  if (!customizationForm) return;
 
-  for (let key in customizations) {
-    const fieldset = Fieldset(key, customizations[key]);
-    customizationForm.insertAdjacentHTML('beforeend', fieldset);
-  }
-}, { once: true });
+  // Output customization fields
+  (() => {
+    const customizationForm = document.querySelector('[data-js="customize"]');
+    if (!customizationForm) return;
 
-// Apply default styles on load
-document.addEventListener('DOMContentLoaded', () => {
-  const defaultValues = [...new FormData(document.querySelector('[data-js="customize"]'))];
-  
-  if (!defaultValues || !defaultValues.length) return;
-
-  defaultValues.forEach(defaultValue => {
-    const [name, value] = defaultValue;
-    document.documentElement.style.setProperty( name, value );
-  });
-}, { once: true });
-
-// Apply new styles on change
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector( '[data-js="customize"]' )?.addEventListener( 'change', e => {
-    const { name, value } = e.target;
-    document.documentElement.style.setProperty( name, value );
-  } );
-}, { once: true });
-
-// Output new styles
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector( '[data-js="customize"]' )?.addEventListener( 'submit', e => {
-    e.preventDefault();
-
-    const formData = new FormData( e.originalTarget );
-
-    let customCss = 'custom-select {\n';
-    for ( let [name, value] of formData ) {
-      customCss += `\t${name}: ${value};\n`;
+    for (let key in customizations) {
+      const fieldset = Fieldset(key, customizations[key]);
+      customizationForm.insertAdjacentHTML('beforeend', fieldset);
     }
-    customCss += '}';
+  })();
 
-    const cssOutputNode = document.querySelector( '[data-js="css-output"]' );
+  // Apply default styles on load
+  (() => {
+    const defaultValues = [...new FormData(document.querySelector('[data-js="customize"]'))];
+  
+    if (!defaultValues || !defaultValues.length) return;
 
-    if ( !cssOutputNode ) return;
+    defaultValues.forEach(defaultValue => {
+      const [name, value] = defaultValue;
+      document.documentElement.style.setProperty( name, value );
+    });
+  })();
 
-    cssOutputNode.value = customCss;
-    console.info( cssOutputNode, customCss )
-    cssOutputNode.select();
-    document.execCommand( 'copy' );
-  } );
-}, { once: true });
+  // Apply new styles on change
+  (() => {
+    document.querySelector( '[data-js="customize"]' )?.addEventListener( 'change', e => {
+      const { name, value } = e.target;
+      document.documentElement.style.setProperty( name, value );
+    } );
+  })();
 
-// Output FormData on submit
-document.addEventListener('DOMContentLoaded', () => {
-  const testForm = document.querySelector( '[data-js="test-form-data"]' );
+  // Output new styles
+  (() => {
+    document.querySelector( '[data-js="customize"]' )?.addEventListener( 'submit', e => {
+      e.preventDefault();
+  
+      const formData = new FormData( e.originalTarget );
+  
+      let customCss = 'custom-select {\n';
+      for ( let [name, value] of formData ) {
+        customCss += `\t${name}: ${value};\n`;
+      }
+      customCss += '}';
+  
+      const cssOutputNode = document.querySelector( '[data-js="css-output"]' );
+  
+      if ( !cssOutputNode ) return;
+  
+      cssOutputNode.value = customCss;
+      console.info( cssOutputNode, customCss )
+      cssOutputNode.select();
+      document.execCommand( 'copy' );
+    } );
+  })();
 
-  if (!testForm) return;
+  // Output FormData on submit
+  (() => {
+    const testForm = document.querySelector( '[data-js="test-form-data"]' );
 
-  testForm.addEventListener( 'submit', e => {
-    e.preventDefault();
-    const testFormData = [...new FormData(testForm)];
-    console.table(testFormData);
-  } );
+    if (!testForm) return;
+
+    testForm.addEventListener( 'submit', e => {
+      e.preventDefault();
+      const testFormData = [...new FormData(testForm)];
+      console.table(testFormData);
+    } );
+  })();
+  
 }, { once: true });
